bd1a72616d9fa5bd1f25db2854e1fc40f6b78fa4
vindiuom96 <vindipererauom@gmail.com>
2019-05-05 22:27:51 +0530

    second commit

--- newjava.java
+++ newjava.java
@@ -1,7 +1,22 @@
 class newjava{
 	
+	public int cal(int c){
+		int total;
+		total= 55 + c;
+		System.out.println(total);
+		
+		
+		return total;
+		
+		
+	}
+	
 	public static void main(String args[]){
 		System.out.println("Hello vindi");
+		
+		newjava cc=new newjava();
+		cc.cal(5);
+		
 	}
 	
 	
